008254fbd647ae412054d664a287aa7b8cb894fe
Ferizal Ramli <ferizalramli@gmail.com>
2026-03-18 17:27:18 -0400

    Fix pylint to 10/10

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Flask server for emotion detection application."""
+
 from flask import Flask, render_template, request
 from EmotionDetection import emotion_detector
 
@@ -6,13 +8,18 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """Render the home page."""
     return render_template('index.html')
 
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """Run emotion detection and return the formatted response."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
 
     return (
         "For the given statement, the system response is "
@@ -27,3 +34,4 @@ def sent_detector():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
